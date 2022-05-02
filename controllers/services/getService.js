66e01dbe2fe423cba62705ccd3e027d661e2c22e
mjwhite10 <manublanco10037@gmail.com>
2022-05-02 03:01:52 +0200

    Añadiendo comments controllers y modificaciones tras testing

--- controllers/services/getService.js
+++ controllers/services/getService.js
@@ -1,15 +1,23 @@
 const { getServiceById } = require('../../db/services');
 const { generateError } = require('../../helpers');
+const { idServiceSchema } = require('../../validators/servicesValidators');
 
 const getService = async (req, res, next) => {
   try {
+    //Validamos los parámetros
+    await idServiceSchema(req.params);
     const { idService } = req.params;
 
     const service = await getServiceById(idService);
 
+    //Comprobamos que existe el servicio
     if (!service)
-      throw generateError(`No existe ningún service con el id ${idService}`, 404);
+      throw generateError(
+        `No existe ningún service con el id ${idService}`,
+        404
+      );
 
+    //Devolvemos la información
     res.send({
       status: 'ok',
       data: service,
